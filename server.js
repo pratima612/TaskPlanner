2214d9b4a08cf7a83f588456aeeac53235c6431c
pratima612 <pratimamali1234@gmail.com>
2026-05-02 00:57:48 +0530

    railway setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files from dist folder
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Handle React routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
